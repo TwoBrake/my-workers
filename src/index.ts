0453e9351af7240d629ef4a6eda68920c34aa3ae
Lucas Stranks <78281961+TwoBrake@users.noreply.github.com>
2025-02-25 15:42:14 -0800

    cleanup code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 // Interfaces
 interface Worker {
   id: string;
+  loop: number;
 }
 
 interface WorkerOptions {
-  interval: number;
+  interval?: number;
 }
 
 /**
@@ -14,7 +15,7 @@ interface WorkerOptions {
 export default class WorkerGroup {
   // Internal variables.
   private id: string;
-  private workers: Worker[];
+  private workers: Worker[] = [];
 
   constructor(id: string) {
     this.id = id;
@@ -25,14 +26,17 @@ export default class WorkerGroup {
    * @param id The ID you want to have for the worker.
    * @param options The options for the worker.
    */
-  add(id: string, options?: WorkerOptions): void {
+  public add(id: string, fn: () => void, options?: WorkerOptions): void {
     const exists = this.workers.find((worker) => worker.id === id);
 
     if (exists)
       throw new Error("A worker with the provided ID already exists.");
 
+    const loop = setInterval(fn, options ? options.interval : 1000);
+
     this.workers.push({
       id: id,
+      loop: loop,
     });
   }
 
@@ -40,16 +44,27 @@ export default class WorkerGroup {
    * Removes a worker from the worker group.
    * @param id The ID to use to find the worker.
    */
-  remove(id: string): void {
+  public remove(id: string): void {
     const found = this.workers.find((worker) => worker.id === id);
 
     if (!found) throw new Error("Could not find worker with given ID.");
+
+    clearInterval(found.loop);
   }
 
   /**
    * Lists all workers from the worker group.
    */
-  list(): Worker[] {
+  public list(): Worker[] {
     return this.workers;
+  }
+
+  /**
+   * Find a specific worker from the worker group.
+   * @param id The ID to use to find the worker.
+   * @return {Worker} The worker that was found.
+   */
+  public find(id: string): Worker | undefined {
+    return this.workers.find((worker) => worker.id === id);
   }
 }
--- src/test/app.ts
+++ src/test/app.ts
@@ -3,5 +3,10 @@ import WorkerGroup from "..";
 
 // Variables
 const endpoints = new WorkerGroup("1");
-endpoints.add("test");
-console.log(endpoints.list());
+endpoints.add("test", () => {
+  console.log("i am a worker!");
+});
+
+setTimeout(() => {
+  endpoints.remove("test");
+}, 5000);
